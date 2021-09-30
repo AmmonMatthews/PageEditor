106b58a891ddbe74c6f4d2399e30af1210580788
AmmonMatthews <ammonmatthews@gmail.com>
2021-09-30 14:44:52 -0600

    working tinyMCE that has been edited

--- src/components/TinyMCE.jsx
+++ src/components/TinyMCE.jsx
@@ -13,16 +13,16 @@ export default function TinyMCE() {
     console.log("Content was updated:", e.target.getContent());
   };
   return (
-    <>
+    <div style={{ width: "50%", margin: "10% auto" }}>
       <Editor
         apiKey="18oa7nai0lyn2rx3ih2878gplnv47kxuxdwe5r52yvurj89a"
         onInit={(evt, editor) => (editorRef.current = editor)}
         initialValue="<p>This is the initial content of the editor.</p>"
         init={{
           height: 500,
-          menubar: true,
+          menubar: false,
           plugins: [
-            "advlist autolink lists  image charmap print preview anchor",
+            "advlist autolink lists link image imageupload imagetoolbar charmap print preview anchor",
             "searchreplace visualblocks code fullscreen",
             "insertdatetime media table paste code help wordcount",
           ],
@@ -30,13 +30,26 @@ export default function TinyMCE() {
             "undo redo | formatselect | " +
             "bold italic backcolor | alignleft aligncenter " +
             "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | help",
+            "link image  |" +
+            "removeformat | fullscreen | help ",
+
           content_style:
             "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
         }}
         onChange={handleEditorChange}
       />
       <button onClick={log}>Log editor content</button>
-    </>
+    </div>
   );
 }
+
+// imagetools_cors_hosts: [
+//             "cloudinary.com",
+//             "shopify.com",
+//             "res.cloudinary.com",
+//           ],
+//           imagetools_credentials_hosts: [
+//             "cloudinary.com",
+//             "shopify.com",
+//             "res.cloudinary.com",
+//           ],
